106baa222bc16ee4edb352279bc9f37d3abb0326
Unknown <sashafp10@ngs.ru>
2017-12-11 00:24:03 +0700

    Tests pass faster

--- ELevator/Elevator.Test/Tests.cs
+++ ELevator/Elevator.Test/Tests.cs
@@ -9,16 +9,16 @@ namespace Elevator.Test
         [TestCase]
         public void Test1()
         {
-            ElevatorController ec = new ElevatorController(10, 4, 2, 5);
+            ElevatorController ec = new ElevatorController(10, 4, 8, 5);
             Assert.AreEqual(1, ec.CurrentFloor, "Starting floor should be equal to 1");
             ec.CommandInitiated(new UserElevatorCommand(3, CommandSources.Elevator));
             ec.CommandInitiated(new UserElevatorCommand(10, CommandSources.Floor));
-            Thread.Sleep(4500);
+            Thread.Sleep(1125);
             ec.CommandInitiated(new UserElevatorCommand(1, CommandSources.Floor));
             Assert.AreEqual(3, ec.CurrentFloor, "Starting floor should be equal to 3");
-            Thread.Sleep(13500);
+            Thread.Sleep(3375);
             Assert.AreEqual(10, ec.CurrentFloor, "Starting floor should be equal to 10");
-            Thread.Sleep(25000);
+            Thread.Sleep(6250);
             Assert.AreEqual(1, ec.CurrentFloor, "Starting floor should be equal to 1");
         }
     }
